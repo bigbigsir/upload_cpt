e1bb3e974115b265c91fbbe4131054acea7018c0
mojie <519628061@qq.com>
2018-10-18 19:01:50 +0800

    【修改人】：莫杰 【修改信息】：

--- js/index.js
+++ js/index.js
@@ -3,3 +3,30 @@
  * Date: 2018/10/11
  */
 
+
+var defaultConfig = {
+    type: "POST",
+    preview: false,
+    multiple: false,
+    autoUpload: true
+};
+
+function init() {
+    console.log(this.options);
+}
+
+function Upload(config) {
+    this.options = $.extend(defaultConfig, config);
+    this.init = init;
+}
+
+
+$(function () {
+    var upload = new Upload({
+        el: "file",
+        url: "/platform/auth/menu/file/upload",
+        success: function (data) {
+            console.log(data);
+        }
+    })
+});
