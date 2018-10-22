3ff26b2bb6506b09acc6dccc7c67cfe33837ad07
mojie <519628061@qq.com>
2018-10-22 23:42:21 +0800

    【修改人】：莫杰 【修改信息】：开发上传组件

--- js/index.js
+++ js/index.js
@@ -21,54 +21,127 @@ function ajax(config) {
     }
 }
 
-
-function init() {
-
-}
-
-function Upload(config) {
-    this.options = $.extend(defaultConfig, config);
-
-}
-
-function insertAfter(newElement, targetElement) { // newElement是要追加的元素 targetElement 是指定元素的位置
-    var parent = targetElement.parentNode; // 找到指定元素的父节点
-    if (parent.lastChild === targetElement) { // 判断指定元素的是否是节点中的最后一个位置 如果是的话就直接使用appendChild方法
+// 在目标元素后插入元素
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild === targetElement) {
         parent.appendChild(newElement);
     } else {
         parent.insertBefore(newElement, targetElement.nextSibling);
     }
-    console.log(targetElement);
 }
 
-function createUploadIframe(id) {
-    var frameId = "uploadFrame" + id;
-    var frame = document.createElement("iframe");
-    frame.id = frameId;
-    frame.name = frameId;
-    frame.className = "upload-frame";
-    document.body.appendChild(frame);
-    return frame;
+// 创建iframe元素
+function createUploadIframe(id, config) {
+    var frameId = "uploadIframe" + id;
+    var iframe = document.createElement("iframe");
+    iframe.id = iframe.name = frameId;
+    iframe.className = "upload-frame";
+    document.body.appendChild(iframe);
+    bindEvent(iframe, "load", function () {
+        var iframe = this;
+        var responseText;
+        var document = iframe.contentWindow || iframe.contentDocument;
+        responseText = document.document.body ? document.document.body.innerText : null;
+        if (responseText && config.success) {
+            config.success(JSON.parse(responseText));
+        }
+    });
+    return iframe;
 }
 
-function createUploadForm(id, fileInputId) {
-    var formId = "uploadFrame" + id;
-    var inputId = "uploadFile" + id;
+// 创建form元素
+function createUploadForm(id, fileInput, config) {
+    var formId = "uploadForm" + id;
     var form = document.createElement("form");
-    var oldInput = document.getElementById(fileInputId);
+    form.id = form.name = formId;
+    form.target = "uploadIframe" + id;
+    form.method = "POST";
+    form.action = config.url;
+    form.enctype = "multipart/form-data";
+    form.className = "upload-form";
+    form.appendChild(fileInput);
+    document.body.appendChild(form);
+    return form;
+}
+
+function createFileInput(id, config) {
+    var inputId = "uploadFile" + id;
+    var oldInput = document.getElementById(config.fileInputId);
     var newInput = oldInput.cloneNode();
+    var span = document.createElement("span");
+    var button = document.createElement("button");
+    span.innerText = config.btnText;
+    button.setAttribute("type", "button");
+    button.className = "el-button";
+    button.appendChild(span);
     oldInput.removeAttribute("name");
     oldInput.style.display = "none";
     newInput.id = inputId;
-    form.id = formId;
-    form.name = formId;
-    form.className = "upload-form";
-    form.appendChild(newInput);
-    insertAfter(form, oldInput);
+    bindEvent(button, "click", function () {
+        newInput.click();
+    });
+    bindEvent(newInput, "change", function () {
+        var form;
+        if (config.autoUpload) {
+            form = document.getElementById("uploadForm" + id);
+            form.submit();
+        }
+    });
+    insertAfter(button, oldInput);
+    return newInput;
 }
 
-$(function () {
+// 添加其他表单参数
+function addOtherRequestsToForm(form, data) {
+    var cloneElement;
+    var originalElement = document.createElement("input");
+    originalElement.type = "hidden";
+    for (var key in data) {
+        cloneElement = originalElement.cloneNode();
+        cloneElement.name = key;
+        cloneElement.value = data[key];
+        form.appendChild(cloneElement);
+    }
+    return form;
+}
+
+// 绑定时间方法
+function bindEvent(target, event, cb) {
+    if (target.addEventListener) {
+        target.addEventListener(event, cb, false)
+    }
+    else if (target.attachEvent) {
+        target.attachEvent('on' + event, cb);
+    }
+    else {
+        target["on" + event] = cb;
+    }
+}
+
+function init(config) {
     var random = Math.random();
-    createUploadIframe(random);
-    createUploadForm(random, "test");
+    var iframe = createUploadIframe(random, config);
+    var fileInput = createFileInput(random, config);
+    var form = createUploadForm(random, fileInput, config);
+    if (config.data) addOtherRequestsToForm(form, config.data);
+}
+
+
+$(function () {
+    init({
+        url: "http://localhost:3000/upload",
+        btnText: "上传",
+        preview: false,
+        multiple: false,
+        autoUpload: true,
+        fileInputId: "test",
+        data: {
+            code: 123,
+            name: "主升浪"
+        },
+        success: function (data) {
+            console.log(data);
+        }
+    })
 });
--- js/upload.js
+++ js/upload.js
@@ -21,6 +21,57 @@ function ajax(config) {
     }
 }
 
+
+
+function upload(config) {
+
+    function insertAfter(newElement, targetElement) {
+        var parent = targetElement.parentNode;
+        if (parent.lastChild === targetElement) {
+            parent.appendChild(newElement);
+        } else {
+            parent.insertBefore(newElement, targetElement.nextSibling);
+        }
+    }
+
+    function createUploadIframe(id) {
+        var frameId = "uploadFrame" + id;
+        var frame = document.createElement("iframe");
+        frame.id = frameId;
+        frame.name = frameId;
+        frame.className = "upload-frame";
+        document.body.appendChild(frame);
+        return frame;
+    }
+
+    function createUploadForm(id, fileInputId) {
+        var formId = "uploadFrame" + id;
+        var inputId = "uploadFile" + id;
+        var form = document.createElement("form");
+        var oldInput = document.getElementById(fileInputId);
+        var newInput = oldInput.cloneNode();
+        oldInput.removeAttribute("name");
+        oldInput.style.display = "none";
+        newInput.id = inputId;
+        form.id = formId;
+        form.name = formId;
+        form.className = "upload-form";
+        form.appendChild(newInput);
+        insertAfter(form, oldInput);
+        return form;
+    }
+
+
+    function init(config) {
+        var random = Math.random();
+        var iframe = createUploadIframe(random);
+        var form = createUploadForm(random, config.fileInputId);
+    }
+
+    config = $.extend(defaultConfig, config);
+    init(config);
+}
+
 var defaultConfig = {
     btnText: "上传",
     preview: false,
@@ -28,12 +79,3 @@ var defaultConfig = {
     autoUpload: true
 };
 
-function init() {
-
-}
-
-function upload(config) {
-
-    config = $.extend(defaultConfig, config);
-    init(config);
-}
