553ce22a38727631923e6e12e6541b77e87e60f9
mojie <519628061@qq.com>
2018-10-22 19:07:15 +0800

    【修改人】：莫杰 【修改信息】：开发上传组件

--- js/index.js
+++ js/index.js
@@ -23,46 +23,52 @@ function ajax(config) {
 
 
 function init() {
-    console.log(this.options);
+
 }
 
 function Upload(config) {
     this.options = $.extend(defaultConfig, config);
-    this.init = init;
+
 }
 
+function insertAfter(newElement, targetElement) { // newElement是要追加的元素 targetElement 是指定元素的位置
+    var parent = targetElement.parentNode; // 找到指定元素的父节点
+    if (parent.lastChild === targetElement) { // 判断指定元素的是否是节点中的最后一个位置 如果是的话就直接使用appendChild方法
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+    console.log(targetElement);
+}
 
+function createUploadIframe(id) {
+    var frameId = "uploadFrame" + id;
+    var frame = document.createElement("iframe");
+    frame.id = frameId;
+    frame.name = frameId;
+    frame.className = "upload-frame";
+    document.body.appendChild(frame);
+    return frame;
+}
+
+function createUploadForm(id, fileInputId) {
+    var formId = "uploadFrame" + id;
+    var inputId = "uploadFile" + id;
+    var form = document.createElement("form");
+    var oldInput = document.getElementById(fileInputId);
+    var newInput = oldInput.cloneNode();
+    oldInput.removeAttribute("name");
+    oldInput.style.display = "none";
+    newInput.id = inputId;
+    form.id = formId;
+    form.name = formId;
+    form.className = "upload-form";
+    form.appendChild(newInput);
+    insertAfter(form, oldInput);
+}
 
 $(function () {
-    $("#btn").on("click", function () {
-        var formData = new FormData();
-
-        for (var i = $("#file")[0].files.length; i--;) {
-            formData.append("file", $("#file")[0].files[i]);
-        }
-
-        console.log(formData);
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:3000/upload",
-            data: formData,
-            processData: false,  // 告诉jQuery不要去处理发送的数据
-            contentType: false,   // 告诉jQuery不要去设置Content-Type请求头
-            success: function (data) {
-                console.log(data)
-            }
-        });
-        // ajax({
-        //     url: "http://localhost:3000/upload",
-        //     data: formData,
-        //     success: function (data) {
-        //         console.log(data)
-        //     }
-        // });
-        // $('#fileForm').submit();
-    });
-    $("#file").on("change", function () {
-        console.log(this.files)
-    });
-
+    var random = Math.random();
+    createUploadIframe(random);
+    createUploadForm(random, "test");
 });
